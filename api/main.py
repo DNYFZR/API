750ee977ed786726d2b3aa9fe699e0ce1c77927c
Daniel <76977155+iDataEngineer@users.noreply.github.com>
2022-07-25 14:37:02 +0100

    Updated API main

--- api/main.py
+++ api/main.py
@@ -1,25 +1,22 @@
+# Postgres API
+import pandas as pd
 from fastapi import FastAPI, Query
-from typing import Union
-import datetime as dt, pandas as pd
+from .postgres import db_pipeline
+
+# Test data
+db = {'atp_tour' : db_pipeline()}
 
 # Initialise Fast API instance
 app = FastAPI()
 
-# Test data
-atp_tour = pd.read_csv(r'/api/data/ATP_tour.csv', index_col=0, parse_dates=['tourney_date'])
-
-db = {'atp_tour' : atp_tour}
-
-# API Build
-
-# Base URL with DB table info
+# Landing Page
 @app.get("/")
 async def read_root():
     return {k: list(v.columns) for k, v in db.items()}
 
 # Get request
 @app.get("/{table_name}/")
-async def read_item(table_name: str, player: str = Query(default=None), year: int = Query(default=None), col: list[str] = Query(default=None)):  
+async def read_item(table_name: str, player: str = Query(default=None), year: int = Query(default=None), col: list[str] = Query(default=None), winner: str = Query(default=None), ):  
     # Select table
     table = db[table_name]
     
@@ -27,9 +24,14 @@ async def read_item(table_name: str, player: str = Query(default=None), year: in
     if year != None:
         table = table[table['tourney_date'].dt.year == year].copy()
 
+    # Filter for winner
+    if winner != None:
+        winner = winner.replace('_', ' ').replace('+', ' ').title()
+        table = table[table['winner_name'] == winner].copy()
+
     # Filter for player
-    if player != None:
-        player = player.replace('_', ' ').replace('+', ' ').replace('&', ' ').title()
+    if player != None and winner == None:
+        player = player.replace('_', ' ').replace('+', ' ').title()
         table = table[(table['winner_name'] == player) | (table['loser_name'] == player)].copy()
 
     # Filter for cols
