4482cd2587b4d25b6304277e0216b4490be786a4
Daniel <76977155+iDataEngineer@users.noreply.github.com>
2022-07-28 21:00:50 +0100

    Added date filtering to main.py API

--- api_postgres/main.py
+++ api_postgres/main.py
@@ -21,7 +21,7 @@ def home():
 
 # Get request
 @api.get('/{db}')
-def get(db: str, table: str, select_col: list[str] = Query(default= None), ):
+def get(db: str, table: str, select_col: list[str] = Query(default= None), date_col: str = Query(default= None), date_from: str = Query(default= None), date_to: str = Query(default= None),):
     '''
         Run query on selected table and return dataframe.
     '''
@@ -33,6 +33,20 @@ def get(db: str, table: str, select_col: list[str] = Query(default= None), ):
             query = query.replace('*', select_col[0])
         else:
             query = query.replace('*', ', '.join(select_col).lstrip(', '))
+
+    # Date fiter
+    if date_col != None and (date_from != None or date_to != None):
+     
+        if date_to == None:
+            cte = f'''WITH temp_table as (SELECT * FROM {table} WHERE EXTRACT(YEAR from {date_col}::DATE) >= {date_from})'''
+     
+        elif date_from == None:
+            cte = f'''WITH temp_table as (SELECT * FROM {table} WHERE EXTRACT(YEAR from {date_col}::DATE) <= {date_to})'''
+     
+        else:
+            cte = f'''WITH temp_table as (SELECT * FROM {table} WHERE EXTRACT(YEAR from {date_col}::DATE) >= {date_from} AND EXTRACT(YEAR from {date_col}::DATE) <= {date_to} )'''
+     
+        query = f'''{cte} {query.replace(table, 'temp_table')}'''
 
     # Run query
     engine = init_engine(db=db)
